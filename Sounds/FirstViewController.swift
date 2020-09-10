b40ab4137ed64c0bbbda491d7d4324d214f4f521
dambarbista1990 <dambarbista.db@gmail.com>
2020-09-09 21:26:19 -0400

    Initial Commit

---
+++ Sounds/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Sounds
+//
+//  Created by Dambar Bista on 9/9/20.
+//  Copyright © 2020 Dambar Bista. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sounds/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Sounds
+//
+//  Created by Dambar Bista on 9/9/20.
+//  Copyright © 2020 Dambar Bista. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
